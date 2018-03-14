7d5a3d5891305e09a640f33f21384bc5d4051560
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2018-03-14 16:30:47 -0400

    Add flash rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def flash_notice
+    return if flash.empty?
+    flash.collect do |key, value|
+      content_tag(:div, value, class: "flash #{key}")
+    end.join.html_safe
+  end
 end
