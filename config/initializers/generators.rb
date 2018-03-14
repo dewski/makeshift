36a7785707a482c36101c3c78ae7398b552aa5b8
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2018-03-14 14:10:42 -0400

    Don't generate all types of files for new models or controllers

---
+++ config/initializers/generators.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.generators do |generate|
+  generate.helper false
+  generate.assets false
+  generate.view_specs false
+end
